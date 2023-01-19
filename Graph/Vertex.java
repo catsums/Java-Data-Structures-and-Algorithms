1ce3e565ccdcf0afe279d0af90605886ef08b46f
Cassim <111745304+catsums@users.noreply.github.com>
2023-01-19 20:52:45 +0200

    Init SimpleGraph pack

---
+++ Graph/Vertex.java
@@ -0,0 +1,63 @@
+
+package Graph;
+
+public class Vertex {
+    private String vName;
+    private Edge[] edges;
+    public Vertex(String vName, int numedges){
+        this.vName = vName;
+        edges = new Edge[numedges];
+    }
+
+    public String getVName(){
+        return vName;
+    }
+
+    public Edge[] getEdges(){
+        return edges;
+    }
+
+    public boolean addEdge(Edge e){
+        if(e == null)
+            return false;
+
+        for(int i=0; i < edges.length; i++)
+        {
+            if(edges[i] == null)
+            {
+                edges[i] = e;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Do not change the above functions
+    //Implement the functions below 
+
+    public boolean isAccessable(Vertex e){
+        return Graph.access(this, e);
+    }
+
+    public Edge getEdgeTo(Vertex vert){
+        for(Edge edge:edges){
+            if(edge==null) continue;
+            if(edge.pointB == vert) return edge;
+        }
+        return null;
+    }
+
+    //////HELPERS//////
+
+    public double dist = 0;
+    public Vertex prev = null;
+    public Vertex pred = null;
+    public double num = Double.POSITIVE_INFINITY;
+    
+    ////remove later
+    // @Override
+    // public String toString(){
+    //     return vName;
+    // }
+
+}
